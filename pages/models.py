b6cc53d99444522d3c5dadd7f00e63884f0a0a98
jawish-a@outlook.com <jawish-a@outlook.com>
2020-08-30 16:08:24 +0300

    complete wiki project

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Page(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    last_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    # def get_absolute_url(self):
+    #     return "/pages/details/%i/" % self.id
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('page-detail', args=[str(self.id)])
